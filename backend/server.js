649aa478f86c74caf409736cfb3c53c9a3c3c19a
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2023-10-16 02:57:54 -0400

    npm update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
