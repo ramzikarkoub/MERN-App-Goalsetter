bab24082a1993b93f0c2a1f853bc2062cf2381f9
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2023-10-17 15:17:45 -0400

    deploy

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "main--mern-app-goalsetter.netlify.app/api/users/";
+const API_URL = "https://main--mern-app-goalsetter.netlify.app/api/users/";
 
 // Register user
 const register = async (userData) => {
