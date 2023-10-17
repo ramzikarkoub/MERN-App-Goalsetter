b47844015259a96777df243e0b5f149ef91e5306
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2023-10-17 14:40:14 -0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,42 @@
 const path = require("path");
-const cors = require("cors");
 const express = require("express");
-const dotenv = require("dotenv").config();
 const colors = require("colors");
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// const app = express();
 // const corsOptions = {
-//   origin: "https://mern-app-goalsetter.vercel.app/",
+//   origin: "http://localhost:3000",
 //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 // };
 
 // app.use(cors(corsOptions));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
-app.use(errorHandler);
-
-app.listen(port, () => console.log(`server running on port ${port}`));
