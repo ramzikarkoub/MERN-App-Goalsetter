1c2e680cfba6cdbe84fda76c2c60f181f19f883c
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2023-10-17 15:51:57 -0400

    deploy

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://main--mern-app-goalsetter.netlify.app", // Replace with your Netlify frontend URL
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Set this to true if your frontend sends cookies or authentication headers
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://main--mern-app-goalsetter.netlify.app", // Replace with your Netlify frontend URL
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true, // Set this to true if your frontend sends cookies or authentication headers
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,17 +25,17 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.use(errorHandler);
 
